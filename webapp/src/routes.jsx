13998017ea57c1823dcd39cf9d1b2a679328b5b9
Omario-r <aviloff@yandex.ru>
2019-04-20 08:13:59 +1000

    add routes

--- webapp/src/routes.jsx
+++ webapp/src/routes.jsx
@@ -3,21 +3,54 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import AppContainer from './js/app/App';
+import AuthUtils from './utils/auth';
+import { setToken } from './utils/fetch';
+import { setUser, fetchSelfUser } from './modules/auth/actions';
+
+// Public components
+import Login from './modules/auth/login';
+// App components
+import AppContainer from './app/App';
+import Users from './modules/users/list';
+import UserForm from './modules/users/form';
 
 const AppRoutes = () => (
   <AppContainer>
-    <div>From routes</div>
+    {/* <Route exact path="/" component={Dashboard} /> */}
+    <Route path="/users" exact={true} component={Users} />
+    <Route path="/users/:id" exact={true} component={UserForm} />
   </AppContainer>
 );
 
+class AuthRoutes extends React.Component {
+  render() {
+    return <div className="appContainer">
+      <Route path="/" component={Login} />
+    </div>
+  }
+}
+
 class Routes extends React.Component {
+  componentDidMount() {
+    const resp = AuthUtils.isLoggedIn();
+    if (resp) {
+      setToken(resp.token);
+      this.props.fetchSelfUser();
+    }
+  }
   render() {
     return <Router>
-      <AppRoutes/>
+      {this.props.auth.user ?
+        <AppRoutes /> :
+        <AuthRoutes />
+      }
     </Router>
   }
 }
 
-export default Routes;
+export default connect(
+  (state) => ({auth: state.auth}), { setUser, fetchSelfUser }
+)(Routes);
+
