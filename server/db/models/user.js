3e3b7e8fca9726cb4bf9b65a74c80b7f944c4aff
Omario-r <aviloff@yandex.ru>
2019-04-20 08:12:26 +1000

    user model

---
+++ server/db/models/user.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    fathers_name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.INTEGER,
+    // roles: 0 - user, 3 - admin
+    active: DataTypes.BOOLEAN,
+    removed: DataTypes.BOOLEAN,
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+  }, {
+    timestamps: false,
+  });
+  return User;
+};
